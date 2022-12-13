856747ec7ef756020b00546cda4936a78a9da3e6
richblomseth <rich.blomseth@gmail.com>
2022-12-12 17:49:21 -0800

    Add missing code.

--- LonTalkStack/Source/Shared/LtPktAllocatorOne.cpp
+++ LonTalkStack/Source/Shared/LtPktAllocatorOne.cpp
@@ -17,3 +17,132 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+#include "LtRouter.h"
+
+#include <LtPktAllocatorOne.h>
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+// LtPktAllocator
+//
+// "Dumb as a post" packet allocator
+// uses malloc / free and new / delete directly.
+//
+
+//
+// constructor
+//
+LtPktAllocatorOne::LtPktAllocatorOne( )
+{
+	m_nBlkSize = MAX_PATH*2;	// assume a long default
+	m_nBlocks = 0;
+	m_sem = semBCreate(0, SEM_FULL);
+}
+
+//
+// destructor
+//
+LtPktAllocatorOne::~LtPktAllocatorOne()
+{
+	semDelete(m_sem);
+}
+
+int LtPktAllocatorOne::getBlockSize()
+{
+	return m_nBlkSize;
+}
+
+void LtPktAllocatorOne::init( int nBlkSize, int nBlks, int nNxtBlks, int nMaxBlks )
+{
+
+	m_nBlkSize = nBlkSize;
+}
+
+
+void LtPktAllocatorOne::initMsgRefs( int nRefs, int nNxtRefs, int nMaxRefs )
+{
+
+
+}
+
+
+void LtPktAllocatorOne::freeAll()
+{
+
+
+}
+
+
+
+// Allocate a block from list, grow if necessary, and NULL if none
+byte*	LtPktAllocatorOne::alloc()
+{
+	if (m_nBlocks < 20)
+	{
+		semTake(m_sem, WAIT_FOREVER);
+		m_nBlocks++;
+		semGive(m_sem);
+		return	(byte*)malloc(m_nBlkSize);
+	}
+	return null;
+}
+
+
+// Allocate a CMsgRef object from a queue somewhere
+LtMsgRef*	LtPktAllocatorOne::allocMsgRef()
+{
+	LtMsgRef*	pRef = new LtPktInfo();
+	pRef->setAllocator( this );
+	return pRef;
+}
+
+
+// Allocate a CMsgRef object and a block and put them together as a master message
+LtMsgRef*	LtPktAllocatorOne::allocMessage()
+{
+	LtMsgRef*	pRef = allocMsgRef();
+	if (pRef != null)
+	{
+		byte*		pBuf = alloc();
+		if (pBuf == null)
+		{
+			free(pRef);
+			return null;
+		}
+		pRef->makeMaster( true );
+		pRef->setBlock( pBuf );
+	}
+	return pRef;
+}
+
+
+// Free a block back to queue, delete if above some threshold, optionally.
+void	LtPktAllocatorOne::free( byte* pBlk )
+{
+	semTake(m_sem, WAIT_FOREVER);
+	m_nBlocks--;
+	semGive(m_sem);
+	::free( pBlk );
+}
+
+
+// Free a message reference object to queue, delete if above some threshold, optionally.
+void	LtPktAllocatorOne::free( LtMsgRef* pMsgRef )
+{
+	delete pMsgRef;
+}
+
+
+// Free a master message to queue, delete if above some threshold, optionally.
+// Performs a CMsgRef.Release() if a client message
+// Performs a Free
+void	LtPktAllocatorOne::freeMessage( LtMsgRef* pMsgRef )
+{
+	pMsgRef->release();
+	free( pMsgRef );
+}
+
+
+
+// end
+
